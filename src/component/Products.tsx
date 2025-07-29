56715e9db077a2764f416280c4eeb2b3fbd2498d
mosesolamide <olamidemoses132@gmail.com>
2025-07-29 02:33:28 +0100

    new

--- src/component/Products.tsx
+++ src/component/Products.tsx
@@ -29,9 +29,8 @@ export default function Products({ size }: { size?: number }): JSX.Element {
         setTouchTimeout(timeout)
     }
 
-    const handleAddToCart = (id: string, e: React.MouseEvent | React.TouchEvent) => {
-        e.stopPropagation() 
-        console.log("Add to cart:", id)
+    const handleAddToCart = (id: string) => {
+        alert(`Add to cart:${id}`)
     }
 
     return (
@@ -39,43 +38,42 @@ export default function Products({ size }: { size?: number }): JSX.Element {
             <Suspense fallback={<Loading />}>
                 <Await resolve={product}>
                     {(furnitureList) => (
-                        <ul className="flex flex-col md:flex-row flex-wrap gap-30 md:gap-12 w-full list-none p-0 m-0">
+                        <ul className="flex flex-col md:flex-row flex-wrap gap-20 md:gap-12 w-full list-none p-0 m-0">
                             {furnitureList.slice(0, size).map((item: FurnitureType): JSX.Element => (
                                 <li key={item.id} className="flex-1">
                                     <article
-                                        className='flex flex-col items-center justify-center relative group hover:cursor-pointer'
+                                        className='flex flex-col items-center justify-center relative
+                                         group hover:cursor-pointer h-[330px] sm:h-[340px] md:h-[310px] '
                                         onTouchStart={() => handleTouchStart(item.id)}
                                         onTouchEnd={handleTouchEnd}
                                         onMouseEnter={() => setTouchedId(item.id)}
                                         onMouseLeave={() => setTouchedId(null)}
                                     >
-                                        <div className='w-50 h-50'>
+                                        <div className='w-50 h-50 z-[10]'>
                                             <img
                                                 src={item.imageUrl}
                                                 alt={item.name}
-                                                className={`w-full h-full object-contain transition-all duration-300 ease-in-out transform ${touchedId === item.id ? 'translate-y-[-19%]' : 'translate-y-0'}`}
+                                                className={`w-full h-full object-contain transition-all 
+                                                duration-300 ease-in-out transform ${touchedId === item.id ? 'translate-y-[-19%]' : 'translate-y-0'}`}
                                             />
                                         </div>
-                                        <div className='flex flex-col items-center py-0.5'>
+                                        <div className='flex flex-col items-center py-0.5 z-[10]'>
                                             <span className='text-gray-900 text-xs lg:text-sm font-medium'>{item.name}</span>
                                             <strong className='text-gray-900 text-xs lg:text-sm font-bold'>${item.price}.00</strong>
                                         </div>
                                         <div
                                             className={`transition-all duration-300 ease-in-out transform h-55 md:h-40 lg:h-50
                                             ${touchedId === item.id ? 'scale-y-100 opacity-100' : 'scale-y-0 opacity-0'} origin-bottom
-                                            bg-gray-300 absolute w-full z-[-10] rounded-xl top-[30%] sm:top-[34%] md:top-20 flex
+                                            bg-gray-300 absolute w-full  rounded-xl top-[30%] sm:top-[34%] md:top-30 flex
                                             justify-center items-center`}
-                                            onClick={(e) => handleAddToCart(item.id, e)}
-                                            onTouchEnd={(e) => {
-                                                handleTouchEnd()
-                                                handleAddToCart(item.id, e)
-                                            }}
                                         >
                                             <img
                                                 src={cross}
+                                                onClick={ () => handleAddToCart(item.id)}
                                                 alt="Add to cart"
-                                                className={`bg-gray-900 p-2 rounded-full transition-all ease-in-out duration-300
-                                                ${touchedId === item.id ? 'opacity-100 translate-y-30 md:translate-y-23' : 'opacity-0 translate-y-0'}`}
+                                                className={`bg-gray-900 p-2 rounded-full transition-all ease-in-out duration-300 
+                                                ${touchedId === item.id ? 'opacity-100 translate-y-30 md:translate-y-23' :
+                                                'opacity-0 translate-y-0'}`}
                                             />
                                         </div>
                                     </article>
