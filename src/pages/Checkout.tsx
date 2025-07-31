a5781dd14e595cc4244989faa44a549f937c8705
mosesolamide <olamidemoses132@gmail.com>
2025-07-31 01:48:54 +0100

    new

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -2,6 +2,17 @@ import type {JSX} from 'react'
 
 export default function Checkout():JSX.Element{
     return(
-        <> checkout</>
+        <div>
+            <div>
+                <h1>Billing Details</h1>
+                <form action="">
+                    <div>
+
+                    </div>
+                </form>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
     )
 }
