38dd30a8351010011b3da5613d6872e669c420aa
mosesolamide <olamidemoses132@gmail.com>
2025-08-02 00:45:56 +0100

    new

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -6,7 +6,7 @@ import SlideShow from '../component/SlideShow'
 export default function Services():JSX.Element {
     return(
         <>
-            <div className='flex mt-22 gap-5 px-4'>
+            <div className='flex flex-col md:flex-row mt-22 gap-5 px-4'>
                 <Features />
                 <Features />
             </div>
